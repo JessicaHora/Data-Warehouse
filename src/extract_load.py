459d1328a634ef8db91cb9d78a5e85de0b634cf3
JessicaHora <jessicahora1234@gmail.com>
2024-06-17 18:57:01 -0300

    adicionar feature de extract e load

---
+++ src/extract_load.py
@@ -0,0 +1,54 @@
+# Import
+import yfinance
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os 
+
+# Import das variáveis de ambiente
+load_dotenv()
+
+commodities = ['CL=F', 'GC=F', 'SI=F']
+
+# Carregar as variáveis de ambiente corretamente
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+# Verifique se todas as variáveis de ambiente estão carregadas corretamente
+assert all([DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS, DB_SCHEMA]), "Uma ou mais variáveis de ambiente não estão definidas."
+
+# Atualizar a URL do banco de dados para incluir o parâmetro sslmode
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+
+engine = create_engine(DATABASE_URL)
+
+# Função para pegar a cotação dos ativos
+def buscar_dados_commodities(simbolo, periodo='5d', intervalo='1d'):
+    ticker = yfinance.Ticker(simbolo)
+    dados = ticker.history(period=periodo,interval=intervalo)[['Close']]
+    dados['simbolo'] = simbolo
+    return dados
+
+# Função para concatenar os dados de todos os ativos
+def buscar_todos_dados_commodities():
+    todos_dados = []
+    for simbolo in commodities:
+        dados = buscar_dados_commodities(simbolo)
+        todos_dados.append(dados)
+    return pd.concat(todos_dados)
+
+# Função para salvar os dados no PostgreSQL
+def salvar_no_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+
+if __name__ == "__main__":
+    dados_concatenados = buscar_todos_dados_commodities()
+    salvar_no_postgres(dados_concatenados, schema=DB_SCHEMA)
+
+
+
+
